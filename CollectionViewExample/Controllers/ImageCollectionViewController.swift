bd24142149bda152228cb1720fd38e097e26d2f6
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-03-02 13:37:20 -0500

    Initial commit

---
+++ CollectionViewExample/Controllers/ImageCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewController.swift
+//  CollectionViewExample
+//
+//  Created by Chris Abbod on 3/2/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ CollectionViewExample/View/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewExample
+//
+//  Created by Chris Abbod on 2/28/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
