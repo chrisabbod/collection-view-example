2674ce7e5a75db1d73a6781981d23ee7ce41710d
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-03-02 14:32:24 -0500

    Create custom collection view cell

---
+++ CollectionViewExample/View/CustomImageCollectionViewCell/ImageCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCollectionViewCell.swift
+//  CollectionViewExample
+//
+//  Created by Chris Abbod on 3/2/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
