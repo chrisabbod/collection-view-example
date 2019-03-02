5c19f41e440427f2b62b7b7b87f9b0303b59f620
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-03-02 13:56:34 -0500

    Add images to project

---
+++ CollectionViewExample/View/Image Collection View Cell/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  CollectionViewExample
+//
+//  Created by Chris Abbod on 3/2/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
